ad5b6f71707cfc69ead9eb4a556cb81c90a87e1d
WPILib Sucks <lukewhite32@icloud.com>
2023-05-12 16:22:38 -0400

    inital. setup everything.

---
+++ main.js
@@ -0,0 +1,89 @@
+var breakfastItems = ['Eggs', 'Bacon', 'Toast', 'Bagel', 'Cereal', 'Parfait', 'Yogurt'];
+var lunchItems = ['Pb and J', 'Tuna melt', 'Turkey sandwich', 'Ham sandwich', 'Roast beef sandwich', 'Item', 'Another item'];
+var dinnerItems = ['Steak', 'Lobster', 'Spagetti', 'Mac n cheese', 'Soup', 'Chili', 'Salmon'];
+
+var sun = [];
+var mon = [];
+var tues = [];
+var wed = [];
+var thurs = [];
+var fri = [];
+var sat = [];
+
+function shuffle(array) {          // thanks stackoverflow
+    /*let currentIndex = array.length,  randomIndex;
+  
+    while (currentIndex != 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+  
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }*/
+    return array.sort(() => Math.random() - .5);
+}
+
+function shuffleItems() {
+    sun = [];
+    mon = [];
+    tues = [];
+    wed = [];
+    thurs = [];
+    fri = [];
+    sat = [];
+    // generate shuffled array and distribute it
+    let b = shuffle(breakfastItems);
+    sun.push(b[0]);
+    mon.push(b[1]);
+    tues.push(b[2]);
+    wed.push(b[3]);
+    thurs.push(b[4]);
+    fri.push(b[5]);
+    sat.push(b[6]);
+    
+    let l = shuffle(lunchItems);
+    sun.push(l[0]);
+    mon.push(l[1]);
+    tues.push(l[2]);
+    wed.push(l[3]);
+    thurs.push(l[4]);
+    fri.push(l[5]);
+    sat.push(l[6]);
+
+    let d = shuffle(dinnerItems);
+    sun.push(d[0]);
+    mon.push(d[1]);
+    tues.push(d[2]);
+    wed.push(d[3]);
+    thurs.push(d[4]);
+    fri.push(d[5]);
+    sat.push(d[6]);
+}
+
+function loadTable() {
+    var item = document.getElementById("main-table");
+    item.innerHTML = "<tr> <th></th> <th>Sunday</th> <th>Monday</th> <th>Tuesday</th> <th>Wednesday</th> <th>Thursday</th> <th>Friday</th> <th>Saturday</th> </tr>";
+ 
+    item.innerHTML += "<tr> <th>Breakfast</th> <td>" + sun[0] + "</td>" + " <td>" + mon[0] + "</td>" + "<td>" + tues[0] + "</td>" + "<td>" + wed[0] + "</td>" + "<td>" + thurs[0] + "</td>" + "<td>" + fri[0] + "</td>" + "<td>" + sat[0] + "</td>" + "</tr>";
+    item.innerHTML += "<tr> <th>Lunch</th><td>" + sun[1] + "</td>" + " <td>" + mon[1] + "</td>" + "<td>" + tues[1] + "</td>" + "<td>" + wed[1] + "</td>" + "<td>" + thurs[1] + "</td>" + "<td>" + fri[1] + "</td>" + "<td>" + sat[1] + "</td>" + "</tr>";
+    item.innerHTML += "<tr> <th>Dinner</th><td>" + sun[2] + "</td>" + " <td>" + mon[2] + "</td>" + "<td>" + tues[2] + "</td>" + "<td>" + wed[2] + "</td>" + "<td>" + thurs[2] + "</td>" + "<td>" + fri[2] + "</td>" + "<td>" + sat[2] + "</td>" + "</tr>";
+}
+
+document.getElementById("shuffle").onclick = () => {
+    shuffleItems();
+    loadTable()
+};
+document.getElementById("add-as-breakfast").onclick = () => {
+    breakfastItems.push(document.getElementById("item-text").value);
+    document.getElementById("item-text").value = ""
+};
+document.getElementById("add-as-lunch").onclick = () => {
+    lunchItems.push(document.getElementById("item-text").value);
+    document.getElementById("item-text").value = ""
+};
+document.getElementById("add-as-dinner").onclick = () => {
+    dinnerItems.push(document.getElementById("item-text").value);
+    document.getElementById("item-text").value = ""
+};
+shuffleItems();
+loadTable();
