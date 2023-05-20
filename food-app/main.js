4588cfb21bbc7128dc1cf586f1ca50a4a81969a2
WPILib Sucks <lukewhite32@icloud.com>
2023-05-20 10:05:31 -0400

    big revamp. still developing

---
+++ food-app/main.js
@@ -0,0 +1,114 @@
+var breakfastItems = ['Eggs', 'Bacon', 'Toast', 'Bagel', 'Cereal', 'Parfait', 'Yogurt'];
+var lunchItems = ['Pb and J', 'Tuna melt', 'Turkey sandwich', 'Ham sandwich', 'Roast beef sandwich', 'Item', 'Another item'];
+var dinnerItems = ['Steak', 'Lobster', 'Spagetti', 'Mac n cheese', 'Soup', 'Chili', 'Salmon'];
+
+var sun = [];
+var mon = [];
+var tues = [];
+var wed = [];
+var thurs = [];
+var fri = [];
+var sat = [];
+
+function shuffle(array) {          // thanks stackoverflow
+    return array.sort(() => Math.random() - .5);
+}
+
+function shuffleItems() {
+    sun = [];
+    mon = [];
+    tues = [];
+    wed = [];
+    thurs = [];
+    fri = [];
+    sat = [];
+    // generate shuffled array and distribute it
+    let b = shuffle(breakfastItems);
+    sun.push(b[0]);
+    mon.push(b[1]);
+    tues.push(b[2]);
+    wed.push(b[3]);
+    thurs.push(b[4]);
+    fri.push(b[5]);
+    sat.push(b[6]);
+    
+    let l = shuffle(lunchItems);
+    sun.push(l[0]);
+    mon.push(l[1]);
+    tues.push(l[2]);
+    wed.push(l[3]);
+    thurs.push(l[4]);
+    fri.push(l[5]);
+    sat.push(l[6]);
+
+    let d = shuffle(dinnerItems);
+    sun.push(d[0]);
+    mon.push(d[1]);
+    tues.push(d[2]);
+    wed.push(d[3]);
+    thurs.push(d[4]);
+    fri.push(d[5]);
+    sat.push(d[6]);
+}
+
+function loadTable() {
+    let item = document.getElementsByClassName("grid-container")[0];
+    //item.innerHTML = "<tr> <th></th> <th>Sunday</th> <th>Monday</th> <th>Tuesday</th> <th>Wednesday</th> <th>Thursday</th> <th>Friday</th> <th>Saturday</th> </tr>";
+    item.innerHTML = '<div class="grid-item"></div>';
+    item.innerHTML += '<div class="grid-item">Sunday</div>';
+    item.innerHTML += '<div class="grid-item">Monday</div>';
+    item.innerHTML += '<div class="grid-item">Tuesday</div>';
+    item.innerHTML += '<div class="grid-item">Wednesday</div>';
+    item.innerHTML += '<div class="grid-item">Thursday</div>';
+    item.innerHTML += '<div class="grid-item">Friday</div>';
+    item.innerHTML += '<div class="grid-item">Saturday</div>';
+
+    item.innerHTML += '<div class="grid-item">Breakfast: </div>';
+    item.innerHTML += '<div class="grid-item">' + sun[0] + '</div>';
+    item.innerHTML += '<div class="grid-item">' + mon[0] + '</div>';
+    item.innerHTML += '<div class="grid-item">' + tues[0] + '</div>';
+    item.innerHTML += '<div class="grid-item">' + wed[0] + '</div>';
+    item.innerHTML += '<div class="grid-item">' + thurs[0] + '</div>';
+    item.innerHTML += '<div class="grid-item">' + fri[0] + '</div>';
+    item.innerHTML += '<div class="grid-item">' + sat[0] + '</div>';
+
+    item.innerHTML += '<div class="grid-item">Lunch: </div>';
+    item.innerHTML += '<div class="grid-item">' + sun[1] + '</div>';
+    item.innerHTML += '<div class="grid-item">' + mon[1] + '</div>';
+    item.innerHTML += '<div class="grid-item">' + tues[1] + '</div>';
+    item.innerHTML += '<div class="grid-item">' + wed[1] + '</div>';
+    item.innerHTML += '<div class="grid-item">' + thurs[1] + '</div>';
+    item.innerHTML += '<div class="grid-item">' + fri[1] + '</div>';
+    item.innerHTML += '<div class="grid-item">' + sat[1] + '</div>';
+
+    item.innerHTML += '<div class="grid-item">Dinner: </div>';
+    item.innerHTML += '<div class="grid-item">' + sun[2] + '</div>';
+    item.innerHTML += '<div class="grid-item">' + mon[2] + '</div>';
+    item.innerHTML += '<div class="grid-item">' + tues[2] + '</div>';
+    item.innerHTML += '<div class="grid-item">' + wed[2] + '</div>';
+    item.innerHTML += '<div class="grid-item">' + thurs[2] + '</div>';
+    item.innerHTML += '<div class="grid-item">' + fri[2] + '</div>';
+    item.innerHTML += '<div class="grid-item">' + sat[2] + '</div>';
+    //item.innerHTML += "<tr> <th>Breakfast</th> <td>" + sun[0] + "</td>" + " <td>" + mon[0] + "</td>" + "<td>" + tues[0] + "</td>" + "<td>" + wed[0] + "</td>" + "<td>" + thurs[0] + "</td>" + "<td>" + fri[0] + "</td>" + "<td>" + sat[0] + "</td>" + "</tr>";
+    //item.innerHTML += "<tr> <th>Lunch</th><td>" + sun[1] + "</td>" + " <td>" + mon[1] + "</td>" + "<td>" + tues[1] + "</td>" + "<td>" + wed[1] + "</td>" + "<td>" + thurs[1] + "</td>" + "<td>" + fri[1] + "</td>" + "<td>" + sat[1] + "</td>" + "</tr>";
+    //item.innerHTML += "<tr> <th>Dinner</th><td>" + sun[2] + "</td>" + " <td>" + mon[2] + "</td>" + "<td>" + tues[2] + "</td>" + "<td>" + wed[2] + "</td>" + "<td>" + thurs[2] + "</td>" + "<td>" + fri[2] + "</td>" + "<td>" + sat[2] + "</td>" + "</tr>";
+}
+
+document.getElementById("shuffle").onclick = () => {
+    shuffleItems();
+    loadTable()
+};
+document.getElementById("add-as-breakfast").onclick = () => {
+    breakfastItems.push(document.getElementById("item-text").value);
+    document.getElementById("item-text").value = ""
+};
+document.getElementById("add-as-lunch").onclick = () => {
+    lunchItems.push(document.getElementById("item-text").value);
+    document.getElementById("item-text").value = ""
+};
+document.getElementById("add-as-dinner").onclick = () => {
+    dinnerItems.push(document.getElementById("item-text"));
+    document.getElementById("item-text").value = ""
+};
+shuffleItems();
+loadTable();
---
+++ games/tictactoe/main.js
@@ -0,0 +1,100 @@
+var rows = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+var player = 1;
+
+function changeSlot(s, val) {
+    rows[s] = val;
+} 
+
+function changePlayer() {
+    if (player == 1) {
+        player = 2;
+        return;
+    }
+    player = 1;
+}
+
+function allEqual(val1, val2, val3) {
+    return (val1 == val3) && (val2 == val3) && (val1 == val2);
+}
+
+function isWin() {
+    for (let x = 0; x < 3; x ++) {
+        if (allEqual(rows[x], rows[x+3], rows[x+6]) && (rows[x] != 0)) {
+            return true;
+        }
+    }
+    for (let x = 0; x < 9; x += 3) {
+        if (allEqual(rows[x], rows[x+1], rows[x+2]) && (rows[x] != 0)) {
+            return true;
+        }
+    }
+    if (allEqual(rows[0], rows[4], rows[8]) && rows[0] != 0) {
+        return true;
+    }
+    if (allEqual(rows[2], rows[4], rows[6]) && rows[2] != 0) {
+        return true;
+    }
+    return false;
+}
+
+function updateBoard() {
+    if (player == 1) {
+        if (isWin()) {
+            document.getElementById("turn").innerText = "O wins!";
+        }
+        else {
+            document.getElementById("turn").innerText = "X's turn";
+            document.getElementById("turn").style.animationPlayState = "paused";
+        }
+    }
+    else {
+        if (isWin()) {
+            document.getElementById("turn").innerText = "X wins!";
+        }
+        else {
+            document.getElementById("turn").style.animationPlayState = "paused";
+            document.getElementById("turn").innerText = "O's turn";
+        }
+    }
+    var board = document.getElementsByClassName("grid-container")[0];
+    
+    board.innerHTML = "";
+
+    for (let x = 0; x < 9; x ++) {
+        let add = document.createElement('DIV');
+        add.className = "grid-item";
+        if (rows[x] == 0) {
+            let butt = document.createElement('BUTTON');
+            butt.className = "click-button";
+            butt.onclick = () => { 
+                changeSlot(x, player);
+                changePlayer();
+                updateBoard();
+            }
+            add.appendChild(butt);
+        }
+        else if (rows[x] == 1) {
+            let x = document.createElement('DIV');
+            x.className = "x-piece";
+            add.appendChild(x);
+        }
+        else {
+            let o = document.createElement('DIV');
+            o.className = "o-piece";
+            add.appendChild(o);
+        }
+        board.appendChild(add);
+    }
+}
+
+function clear() {
+    rows = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    player = 1;
+    document.getElementById("turn").style.animationPlayState = "paused";
+}
+
+document.getElementById("c").onclick = () => {
+    clear();
+    updateBoard();
+}
+updateBoard();
