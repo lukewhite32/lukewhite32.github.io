e0e7c88d9e03c83b736d62d6ee84a5d24cc2feda
WPILib Sucks <lukewhite32@icloud.com>
2023-06-22 21:54:53 -0400

    revamp more stuff

--- food-app/main.js
+++ food-app/main.js
@@ -1,129 +1,181 @@
-var breakfastItems = ['Eggs', 'Bacon', 'Toast', 'Bagel', 'Cereal', 'Parfait', 'Yogurt'];
-var lunchItems = ['Pb and J', 'Tuna melt', 'Turkey sandwich', 'Ham sandwich', 'Roast beef sandwich', 'Item', 'Another item'];
-var dinnerItems = ['Steak', 'Lobster', 'Spagetti', 'Mac n cheese', 'Soup', 'Chili', 'Salmon'];
-var webSock = new WebSocket("ws://localhost:54321");
+var ISTESTING = true;
 
-var sun = [];
-var mon = [];
-var tues = [];
-var wed = [];
-var thurs = [];
-var fri = [];
-var sat = [];
+var webSock = 0;
+if (ISTESTING) {
+    webSock = new WebSocket("ws://localhost:54321");
+}
+else {
+    webSock = new WebSocket("wss://swaous.asuscomm.com/pookie/foodapp/foods");
+}
+
+var lastMessage = "";
+
+var breakfastItems = [];
+var lunchItems = [];
+var dinnerItems = [];
+
+var allBreakfast = [];
+var allLunch = [];
+var allDinner = [];
+
+var hasLoggedIn = false;
+var isUpdatedItems = false;
+
+var invalidCharacters = [';', ':'];
 
 function shuffle(array) {          // thanks stackoverflow
     return array.sort(() => Math.random() - .5);
 }
 
-function shuffleItems() {
-    sun = [];
-    mon = [];
-    tues = [];
-    wed = [];
-    thurs = [];
-    fri = [];
-    sat = [];
-    // generate shuffled array and distribute it
-    let b = shuffle(breakfastItems);
-    sun.push(b[0]);
-    mon.push(b[1]);
-    tues.push(b[2]);
-    wed.push(b[3]);
-    thurs.push(b[4]);
-    fri.push(b[5]);
-    sat.push(b[6]);
-    
-    let l = shuffle(lunchItems);
-    sun.push(l[0]);
-    mon.push(l[1]);
-    tues.push(l[2]);
-    wed.push(l[3]);
-    thurs.push(l[4]);
-    fri.push(l[5]);
-    sat.push(l[6]);
-
-    let d = shuffle(dinnerItems);
-    sun.push(d[0]);
-    mon.push(d[1]);
-    tues.push(d[2]);
-    wed.push(d[3]);
-    thurs.push(d[4]);
-    fri.push(d[5]);
-    sat.push(d[6]);
-}
+webSock.addEventListener("message", (message) => {
+    lastMessage = message.data;
+});
 
 function loadTable() {
-    let item = document.getElementsByClassName("grid-container")[0];
+    let table = document.getElementsByClassName("grid-container")[0];
+    let removeBreakfast = document.getElementById("breakfast-optgroup");
+    let removeLunch = document.getElementById("lunch-optgroup");
+    let removeDinner = document.getElementById("dinner-optgroup");
+    removeBreakfast.innerHTML = "";
+    removeLunch.innerHTML = "";
+    removeDinner.innerHTML = "";
     //item.innerHTML = "<tr> <th></th> <th>Sunday</th> <th>Monday</th> <th>Tuesday</th> <th>Wednesday</th> <th>Thursday</th> <th>Friday</th> <th>Saturday</th> </tr>";
-    item.innerHTML = '<div class="grid-item"></div>';
-    item.innerHTML += '<div class="grid-item">Sunday</div>';
-    item.innerHTML += '<div class="grid-item">Monday</div>';
-    item.innerHTML += '<div class="grid-item">Tuesday</div>';
-    item.innerHTML += '<div class="grid-item">Wednesday</div>';
-    item.innerHTML += '<div class="grid-item">Thursday</div>';
-    item.innerHTML += '<div class="grid-item">Friday</div>';
-    item.innerHTML += '<div class="grid-item">Saturday</div>';
+    table.innerHTML = '<div class="grid-item"></div>';
+    table.innerHTML += '<div class="grid-item">Sunday</div>';
+    table.innerHTML += '<div class="grid-item">Monday</div>';
+    table.innerHTML += '<div class="grid-item">Tuesday</div>';
+    table.innerHTML += '<div class="grid-item">Wednesday</div>';
+    table.innerHTML += '<div class="grid-item">Thursday</div>';
+    table.innerHTML += '<div class="grid-item">Friday</div>';
+    table.innerHTML += '<div class="grid-item">Saturday</div>';
 
-    item.innerHTML += '<div class="grid-item">Breakfast: </div>';
-    item.innerHTML += '<div class="grid-item">' + sun[0] + '</div>';
-    item.innerHTML += '<div class="grid-item">' + mon[0] + '</div>';
-    item.innerHTML += '<div class="grid-item">' + tues[0] + '</div>';
-    item.innerHTML += '<div class="grid-item">' + wed[0] + '</div>';
-    item.innerHTML += '<div class="grid-item">' + thurs[0] + '</div>';
-    item.innerHTML += '<div class="grid-item">' + fri[0] + '</div>';
-    item.innerHTML += '<div class="grid-item">' + sat[0] + '</div>';
+    table.innerHTML += '<div class="grid-item">Breakfast: </div>';
 
-    item.innerHTML += '<div class="grid-item">Lunch: </div>';
-    item.innerHTML += '<div class="grid-item">' + sun[1] + '</div>';
-    item.innerHTML += '<div class="grid-item">' + mon[1] + '</div>';
-    item.innerHTML += '<div class="grid-item">' + tues[1] + '</div>';
-    item.innerHTML += '<div class="grid-item">' + wed[1] + '</div>';
-    item.innerHTML += '<div class="grid-item">' + thurs[1] + '</div>';
-    item.innerHTML += '<div class="grid-item">' + fri[1] + '</div>';
-    item.innerHTML += '<div class="grid-item">' + sat[1] + '</div>';
+    for (let x = 0; x < 7; x ++) {
+        table.innerHTML += '<div class="grid-item">' + breakfastItems[x] + '</div>';
+        let item = document.createElement('option');
+        item.value = "b:" + breakfastItems[x];
+        item.innerText = breakfastItems[x];
+        removeBreakfast.appendChild(item);
+    }
 
-    item.innerHTML += '<div class="grid-item">Dinner: </div>';
-    item.innerHTML += '<div class="grid-item">' + sun[2] + '</div>';
-    item.innerHTML += '<div class="grid-item">' + mon[2] + '</div>';
-    item.innerHTML += '<div class="grid-item">' + tues[2] + '</div>';
-    item.innerHTML += '<div class="grid-item">' + wed[2] + '</div>';
-    item.innerHTML += '<div class="grid-item">' + thurs[2] + '</div>';
-    item.innerHTML += '<div class="grid-item">' + fri[2] + '</div>';
-    item.innerHTML += '<div class="grid-item">' + sat[2] + '</div>';
+    table.innerHTML += '<div class="grid-item">Lunch: </div>';
+
+    for (let x = 0; x < 7; x ++) {
+        table.innerHTML += '<div class="grid-item">' + lunchItems[x] + '</div>';
+        let item = document.createElement('option');
+        item.value = "l:" + lunchItems[x];
+        item.innerText = lunchItems[x];
+        removeLunch.appendChild(item);
+    }
+
+    table.innerHTML += '<div class="grid-item">Dinner: </div>';
+
+    for (var x = 0; x < 7; x ++) {
+        table.innerHTML += '<div class="grid-item">' + dinnerItems[x] + '</div>';
+        let item = document.createElement('option');
+        item.value = "d:" + dinnerItems[x];
+        item.innerText = dinnerItems[x];
+        removeDinner.appendChild(item);
+    }
     //item.innerHTML += "<tr> <th>Breakfast</th> <td>" + sun[0] + "</td>" + " <td>" + mon[0] + "</td>" + "<td>" + tues[0] + "</td>" + "<td>" + wed[0] + "</td>" + "<td>" + thurs[0] + "</td>" + "<td>" + fri[0] + "</td>" + "<td>" + sat[0] + "</td>" + "</tr>";
     //item.innerHTML += "<tr> <th>Lunch</th><td>" + sun[1] + "</td>" + " <td>" + mon[1] + "</td>" + "<td>" + tues[1] + "</td>" + "<td>" + wed[1] + "</td>" + "<td>" + thurs[1] + "</td>" + "<td>" + fri[1] + "</td>" + "<td>" + sat[1] + "</td>" + "</tr>";
     //item.innerHTML += "<tr> <th>Dinner</th><td>" + sun[2] + "</td>" + " <td>" + mon[2] + "</td>" + "<td>" + tues[2] + "</td>" + "<td>" + wed[2] + "</td>" + "<td>" + thurs[2] + "</td>" + "<td>" + fri[2] + "</td>" + "<td>" + sat[2] + "</td>" + "</tr>";
 }
-document.getElementById("login-button").onclick = () => {
-    if (document.getElementById("login-username").value == "ellas-use-only") {
-        if (document.getElementById("login-password").value == "tHiSiSsEcUrE") {
-            document.getElementById("everything").style.display = "block";
-            document.getElementById("login").style.display = "none";
-            document.getElementById("login").style.position = "absolute";
-        }
-        else {
-            alert("LLLLLLLLLLLLLLLLLLLL! You tried, but failed XD");
+
+setInterval(function () {
+    if (webSock.readyState == WebSocket.CLOSED) {
+        window.location.href = "no-conn";
+    }
+    if (!isUpdatedItems) {
+        webSock.send("getAll");
+        if (lastMessage[0] == 'A') {
+            allBreakfast = [];
+            allLunch = [];
+            allDinner = [];
+            let items = lastMessage.split(";");
+            for (let x = 1; x < items.length; x ++) {
+                if (items[x][0] == 'b') {
+                    allBreakfast.push(items[x].split(":")[1]);
+                }
+                else if (items[x][0] == 'l') {
+                    allLunch.push(items[x].split(":")[1]);
+                }
+                else if (items[x][0] == 'd') {
+                    allDinner.push(items[x].split(":")[1]);
+                }
+            }
+            isUpdatedItems = true;
         }
     }
     else {
-        alert("LLLLLLLLLLLLLLLLLLLL! You tried, but failed XD");
+        if (hasLoggedIn) {
+            webSock.send("get");
+        }
+    }
+    let items = lastMessage.split("\n"); 
+    items.splice(21);
+    if (items.length != 1) {
+        breakfastItems = [];
+        lunchItems = [];
+        dinnerItems = [];
+        for (let x = 0; x < 7; x ++) {
+            breakfastItems.push(items[x].split(":")[1]);
+        }
+        for (let x = 7; x < 14; x ++) {
+            lunchItems.push(items[x].split(":")[1]);
+        }
+        for (let x = 14; x < 21; x ++) {
+            dinnerItems.push(items[x].split(":")[1]);
+        }
+    }
+}, 50);
+
+var loginInterval = 0;
+
+function checkForLogin(data) {
+    if (hasLoggedIn) {
+        return;
+    }
+    webSock.send("up:" + data);
+    if (lastMessage[0] + lastMessage[1] == "up" && (!hasLoggedIn)) {
+        if (lastMessage.split(":")[1] == "1") {
+            document.getElementById("everything").style.display = "block";
+            document.getElementById("login").style.display = "none";
+            document.getElementById("login").style.position = "absolute";
+            hasLoggedIn = true;
+            document.title = "Food app - Items";
+        }
+        else {
+            clearInterval(loginInterval);
+            alert("Username or password not correct. Please try again.");
+        }
     }
 }
+
+document.getElementById("login-button").onclick = () => {
+    loginInterval = setInterval(function () {
+        checkForLogin(document.getElementById("login-username").value + document.getElementById("login-password").value);
+    }, 50);
+}
+
 document.getElementById("shuffle").onclick = () => {
-    shuffleItems();
-    loadTable()
+    webSock.send("shuffle");
+    loadTable();
 };
 document.getElementById("add-as-breakfast").onclick = () => {
-    breakfastItems.push(document.getElementById("item-text").value);
+    webSock.send("a;b:" + document.getElementById("item-text").value);
     document.getElementById("item-text").value = ""
 };
 document.getElementById("add-as-lunch").onclick = () => {
-    lunchItems.push(document.getElementById("item-text").value);
+    webSock.send("a;l:" + document.getElementById("item-text").value);
     document.getElementById("item-text").value = ""
 };
 document.getElementById("add-as-dinner").onclick = () => {
-    dinnerItems.push(document.getElementById("item-text"));
+    webSock.send("a;d:" + document.getElementById("item-text").value);
     document.getElementById("item-text").value = ""
 };
-shuffleItems();
+document.getElementById("remove-button").onclick = () => {
+    webSock.send("r;" + document.getElementById("remove-select").value);
+}
 loadTable();
