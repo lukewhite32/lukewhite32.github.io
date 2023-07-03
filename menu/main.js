163390ea80a22d9855650428af2519e082cff624
WPILib Sucks <lukewhite32@icloud.com>
2023-07-03 18:18:14 -0400

    add new suggestion stuff

--- menu/main.js
+++ menu/main.js
@@ -1,4 +1,4 @@
-var webSock = new WebSocket("ws://localhost:54321"); //("wss://swaous.asuscomm.com/pookie/foodapp/foods");
+var webSock = new WebSocket("wss://swaous.asuscomm.com/pookie/foodapp/foods");
 
 function highlightButton(buttonObj) {
     buttonObj.style.backgroundColor = "white";
@@ -12,15 +12,33 @@ function resetButtonColors(buttonObj) {
 
 webSock.addEventListener('message', function (message) {
     if (message.data == "S:0") {
-        document.getElementById("cooldown").innerHTML = "Wait! You are submitting reports too fast!";
+        document.getElementById("submit-error").innerHTML = "Wait! You are submitting reports too fast!";
     }
     else {
-        document.getElementById("cooldown").value = "";
+        document.getElementById("submit-error").style.color = "green";
+        document.getElementById("submit-error").innerHTML = "Submit successful";
     }
 });
 
 function submitReport(text) {
+    document.getElementById("submit-error").style.color = "red";
     if (text != "") {
-        webSock.send("S:"+text);
+        if (text.length > 120) {
+            document.getElementById("submit-error").innerHTML = "Your suggestion cannot exceed 120 characters! Current: " + text.length;
+        }
+        else {
+            webSock.send("S:"+text);
+            var sendDelay = false;
+            var submitInterval = setInterval(function () {
+                if (sendDelay) {
+                    if (document.getElementById("submit-error").innerHTML == "Sending suggestion...") {
+                        document.getElementById("submit-error").innerHTML = "Could not access server at this minute!";
+                    }
+                    clearInterval(submitInterval);
+                }
+                sendDelay = true;
+            }, 1000)
+            document.getElementById("submit-error").innerHTML = "Sending suggestion...";
+        }
     }
 }
---
+++ view-suggestions/main.js
@@ -0,0 +1,53 @@
+var webSock = new WebSocket("wss://swaous.asuscomm.com/pookie/foodapp/foods");
+
+var hasItems = false;
+var isGathering = false;
+var allSuggestions = [];
+webSock.addEventListener('message', function (message) {
+    if (isGathering) {
+        if (message.data.substring(0, 3) == "S?:") {
+            if (message.data == "S?:EOF") {
+                isGathering = false;
+                hasItems = true;
+                webSock.send("S?Y");
+            }
+            else {
+                let i = message.data.split("S?:")[1];
+                if ((i != "") || (i != "")) {
+                    allSuggestions.push(i);
+                }
+                webSock.send("S?");
+            }
+        }
+
+    }
+});
+
+function displaySuggestions() {
+    if (webSock.readyState == WebSocket.CLOSED) {
+        document.getElementById("conn-status").innerText = "Loading...";
+    }
+    else {
+        document.getElementById("conn-status").innerText = "";
+    }
+    if (!isGathering && !hasItems) {
+        allSuggestions = [];
+        webSock.send("S?");
+        isGathering = true;
+    }
+    if (hasItems) {
+        document.getElementById("suggestions").innerHTML = "";
+        allSuggestions.forEach(item => {
+            let c = document.createElement('div');
+            c.className = "content";
+            c.innerText = item;
+            document.getElementById("suggestions").appendChild(c);
+        });
+        if (document.getElementById("suggestions").innerHTML == "") {
+            document.getElementById("suggestions").innerHTML = "(No suggestions have been made)";
+        }
+    }
+
+}
+
+setInterval(displaySuggestions, 400);
